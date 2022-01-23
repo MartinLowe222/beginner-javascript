acb107a575546d43f1c958431cefd0479169a32e
MartinAardy <Martin@Aardy.com>
2022-01-23 18:19:47 +0000

    3/4 done

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,26 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+// 1st grab the tabs secopnd grab the buttons 
+
+const tabs  = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+
+function handleTabClick(event){
+    // hide all tab panels when someone clicks 
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    });
+    //mark all tabs as unselected
+    tabButtons.forEach(tab => {
+        //tab.ariaSelected = false;
+        tab.setAttribute('aria-selected', false);
+    });
+    // mark the clicked tab as selected
+event.currentTarget.setAttribute("aria-selected", true);
+
+
+    // find the associated tab panel and show it 
+    console.log(event.currentTarget);
+}
+
+tabButtons.forEach(button => button.addEventListener("click", handleTabClick));
