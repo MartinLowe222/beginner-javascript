7a268eb5eff3ee0d388fe187618e81720c970cb0
MartinAardy <Martin@Aardy.com>
2022-03-22 16:57:10 +0000

    hoisting

--- playground/hoisting.js
+++ playground/hoisting.js
@@ -1,6 +1,21 @@
+// allows you to access fucntions and variables before they have been created
+// just think hoisting works variable declartion and functions declartions hoists them to the top 
+console.log("hi");
+
+
+console.log(age);
+var age = 10;
+// this does not work you can use hoisting to see if varaibles are created 
+console.log(age);
+
+// javascript will move all your functions to top so techinally you can run a function before its exists 
 sayHi();
 
 function sayHi(){
     console.log("hey!");
+    console.log(add(10,2));
 }
 
+function add(a, b){
+    return a+ b;
+}
